aea4348f45256bada09a33eb0c9fe08f4e7247eb
Joseperez <51133263+joseperu2503@users.noreply.github.com>
2023-08-20 13:44:59 -0500

    Update CharacterItem.vue

--- src/components/character/CharacterItem.vue
+++ src/components/character/CharacterItem.vue
@@ -1,20 +1,19 @@
 <template>
-  <div @click="goCharacter()" class="relative cursor-pointer hover:scale-110 transition-all w-full aspect-square"
+  <div @click="goCharacter()" class="relative cursor-pointer hover:scale-110 transition-all w-full aspect-square overflow-hidden rounded-2xl"
     :class="{ 'animate-pulse': !imageLoaded && !imageError }">
-    <img src="@/assets/avatar_default.jpeg" class="rounded-2xl w-full"
+    <img src="@/assets/avatar_default.jpeg" class="w-full absolute"
       v-if="imageError || (!imageLoaded && !imageError)" />
-    <img :src="character.image" class="rounded-2xl w-full" @load="onImageLoad" @error="onImageError"
-      v-show="imageLoaded" />
-    <div class="absolute bg-gradient-to-t from-black bottom-0 h-1/5 w-full rounded-b-2xl">
+    <img :src="character.image" class="w-full absolute" @load="onImageLoad" @error="onImageError"
+      v-show="imageLoaded"/>
+    <div class="absolute bg-gradient-to-t from-black bottom-0 h-1/5 w-full ">
     </div>
-    <div class="absolute bg-gradient-to-t from-black bottom-0 opacity-50 h-2/5 w-full rounded-b-2xl">
+    <div class="absolute bg-gradient-to-t from-black bottom-0 opacity-50 h-2/5 w-full">
     </div>
-    <div class="absolute bottom-0 h-2/5 w-full px-4 flex items-end pb-2 sm:pb-4 rounded-b-2xl">
+    <div class="absolute bottom-0 h-2/5 w-full px-4 flex items-end pb-2 sm:pb-4">
       <h2 class="text-lg sm:text-xl md:text-xl text-rick-white leading-none line-clamp-2">
         {{ character.name }}
       </h2>
     </div>
-
   </div>
 </template>
 <script lang="ts" setup>
@@ -38,6 +37,7 @@ const goCharacter = () => {
 
 const onImageLoad = () => {
   imageLoaded.value = true;
+  imageError.value = false;
 };
 
 const onImageError = () => {
