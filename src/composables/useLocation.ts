cbe1cd632c2a3c20c57555d55447701aac84b9e4
Joseperez <51133263+joseperu2503@users.noreply.github.com>
2023-08-28 20:14:54 -0500

    location skeleton

--- src/composables/useLocation.ts
+++ src/composables/useLocation.ts
@@ -6,36 +6,38 @@ import { useSomeCharacters } from "@/composables/useSomeCharacters";
 
 export function useLocation() {
   const location = ref<Location>(initLocation);
-
+  const loadingLocation = ref<boolean>(true);
+  const loadingCharacters = ref<boolean>(true);
   const { characters, getSomeCharacters } = useSomeCharacters();
 
-  const getLocation = (locationId: string) => {
-    http
-      .get<Location>(`location/${locationId}`)
-      .then((response) => {
-        location.value = response.data;
+  const getLocation = async (locationId: string) => {
+    loadingLocation.value = true;
+    loadingCharacters.value = true
 
-        let charactersId: string[] = [];
+    let response = await http.get<Location>(`location/${locationId}`)
+    location.value = response.data;
+    loadingLocation.value = false
 
-        location.value.residents.map((url) => {
-          const parts = url.split("/");
-          const id = parts[parts.length - 1];
-          charactersId.push(id);
-        });
+    let charactersId: string[] = [];
 
-        if (charactersId.length > 0) {
-          let someCharacters = charactersId.join(",");
-          getSomeCharacters(someCharacters);
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    location.value.residents.map((url) => {
+      const parts = url.split("/");
+      const id = parts[parts.length - 1];
+      charactersId.push(id);
+    });
+
+    if (charactersId.length > 0) {
+      let someCharacters = charactersId.join(",");
+      await getSomeCharacters(someCharacters);
+      loadingCharacters.value = false
+    }
   };
 
   return {
     getLocation,
     location,
     characters,
+    loadingLocation,
+    loadingCharacters,
   };
 }
--- src/views/Location.vue
+++ src/views/Location.vue
@@ -11,20 +11,31 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large" class="title">{{ location.name }}</ion-title>
+          <ion-title size="large" class="title">
+            <div v-if="loadingLocation" class="w-40 h-7 mb-[12px] animate-pulse bg-white/50 rounded-lg" />
+            <template v-else>
+              {{ location.name }}
+            </template>
+          </ion-title>
         </ion-toolbar>
       </ion-header>
       <div class="ion-padding">
         <div class="flex mb-10 gap-2 items-center justify-start">
-          <div class="border-rick-3 border px-2 py-1 rounded-lg text-rick-3 text-xs font-medium">
-            {{ location.dimension }}
-          </div>
-          <div class="border-rick-green-1 border px-2 py-1 rounded-lg text-rick-white text-xs font-medium">
-            {{ location.type }}
-          </div>
+          <template v-if="loadingLocation">
+            <div class="w-[100px] h-[26px] animate-pulse bg-rick-3/70 rounded-lg" />
+            <div class="w-[90px] h-[26px] animate-pulse bg-rick-green-1/70 rounded-lg" />
+          </template>
+          <template v-else>
+            <div class="border-rick-3 border px-2 py-1 rounded-lg text-rick-3 text-xs font-medium">
+              {{ location.dimension }}
+            </div>
+            <div class="border-rick-green-1 border px-2 py-1 rounded-lg text-rick-white text-xs font-medium">
+              {{ location.type }}
+            </div>
+          </template>
         </div>
-
-        <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+        <CharacterSkeleton v-if="loadingCharacters" />
+        <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4" v-else>
           <CharacterItem v-for="item in characters" :key="item.id" :character="item" />
         </div>
       </div>
@@ -36,20 +47,22 @@ import CharacterItem from "@/components/character/CharacterItem.vue";
 import { useLocation } from "@/composables/useLocation";
 import { useRoute } from "vue-router";
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonPage, IonBackButton, IonButtons } from '@ionic/vue';
+import CharacterSkeleton from "@/components/shared/CharacterSkeleton.vue";
 
 const route = useRoute();
 
 const locationId = route.params.locationId as string;
 
-const { location, getLocation, characters } = useLocation();
+const { location, getLocation, characters, loadingCharacters, loadingLocation } = useLocation();
 getLocation(locationId);
 
 </script>
 <style scoped>
-.ion-padding{
+.ion-padding {
   padding-top: 0px;
 }
-.title{
+
+.title {
   font-size: 28px;
 }
 </style>
