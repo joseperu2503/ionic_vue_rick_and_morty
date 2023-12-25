d3be83451440a21d8e02b63d13ec9f97901c8525
Joseperez <51133263+joseperu2503@users.noreply.github.com>
2023-12-25 16:07:47 -0500

    configuracion identificador app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.joseperezgil.rickandmortyionic', //configuracion identificador app
   appName: 'Rick and Morty',
   webDir: 'dist',
   server: {
