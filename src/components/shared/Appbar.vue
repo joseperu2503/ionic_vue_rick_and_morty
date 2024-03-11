0b893a417c3becc7c10b617672d5021f0ee24800
joseperu2503 <joseperu2503@gmail.com>
2024-03-10 21:49:02 -0500

    Update Appbar.vue

--- src/components/shared/Appbar.vue
+++ src/components/shared/Appbar.vue
@@ -3,9 +3,19 @@
     <ion-title>{{ title }}</ion-title>
     <ion-buttons slot="primary">
       <ion-button @click="openAuthModal" v-if="!user">
-        <ion-icon slot="icon-only" :icon="personCircle" color="light"></ion-icon>
+        <ion-icon
+          slot="icon-only"
+          :icon="personCircle"
+          class="text-gray-300"
+        ></ion-icon>
       </ion-button>
-      <ion-button @click="openAuthModal" fill="solid" shape="round" class="aspect-square profile" v-else>
+      <ion-button
+        @click="openAuthModal"
+        fill="solid"
+        shape="round"
+        class="aspect-square profile"
+        v-else
+      >
         {{ nameInitial }}
       </ion-button>
     </ion-buttons>
@@ -13,29 +23,33 @@
 </template>
 
 <script setup lang="ts">
-import { IonTitle, IonToolbar, IonButtons, IonButton, IonIcon } from '@ionic/vue';
-import { personCircle } from 'ionicons/icons';
-import { useAuthModal } from '@/composables/useAuthModal';
-import { useAuthStore } from '@/stores/auth'
-import { storeToRefs } from 'pinia';
+import {
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonButton,
+  IonIcon,
+} from "@ionic/vue";
+import { personCircle } from "ionicons/icons";
+import { useAuthModal } from "@/composables/useAuthModal";
+import { useAuthStore } from "@/stores/auth";
+import { storeToRefs } from "pinia";
 
-const authStore = useAuthStore()
+const authStore = useAuthStore();
 const { nameInitial, user } = storeToRefs(authStore);
 defineProps<{ title: string }>();
 
-const { openAuthModal } = useAuthModal()
-
+const { openAuthModal } = useAuthModal();
 </script>
 <style scoped>
 ion-button.profile {
-  --background: #5CAD4A;
-  --background-activated: #208D45;
-  --color: #F0F2EB;
+  --background: #5cad4a;
+  --background-activated: #208d45;
+  --color: #f0f2eb;
 }
 
 ion-toolbar {
   --padding-end: 12px;
   --padding-start: 12px;
-
 }
 </style>
